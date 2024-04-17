4029df77f7555610bfaf7582d966b1784a908dd0
Ivan Smetanin <ivan.smetanin100@gmail.com>
2024-04-17 20:19:56 +0300

    Начал делать контроллер для юзера и сделал в постмане группу с запросами к юзеру

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const { UserController } = require('../controllers');
 
 const uploadDestination = 'uploads';
 
@@ -14,8 +15,9 @@ const storage = multer.diskStorage({
 
 const uploads = multer({ storage: storage });
 
-router.get('/register', (req, res) => {
-  res.send('register');
-});
-
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.get('/current', UserController.current);
+router.get('/users/:id', UserController.getUserById);
+router.put('/users/:id', UserController.updateUser);
 module.exports = router;
