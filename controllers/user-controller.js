c614ede9106d6dba5067066871f388fb943107b4
Ivan Smetanin <ivan.smetanin100@gmail.com>
2024-04-19 21:05:41 +0300

    Написал контроллер для регистрации и почти для логина

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,60 @@
+const { prisma } = require('../prisma/prisma-client');
+const bcrypt = require('bcryptjs');
+const Jdenticon = require('jdenticon');
+const path = require('path');
+const fs = require('fs');
+const { error } = require('console');
+const jwt = require('jsonwebtoken');
+
+const createError = { error: 'Все поля обязательны' };
 const UserController = {
   register: async (req, res) => {
-    res.send('register');
+    const { email, password, name } = req.body;
+    if (!email || !password || !name) {
+      return res.status(400).json(createError);
+    }
+    try {
+      const existingUser = await prisma.user.findUnique({ where: { email } });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Пользователь уже существует' });
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      //генерируем pgn аватарку для пользователя , 200 число написано в документации
+      const png = Jdenticon.toPng(name, 200);
+      const avatarName = `${name}_${Date.now()}.png`;
+      const avatarPath = path.join(__dirname, '../uploads', avatarName);
+      fs.writeFileSync(avatarPath, png);
+      const user = await prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          name,
+          avatarUrl: `/uploads/${avatarName}`,
+        },
+      });
+      res.json(user);
+    } catch (error) {
+      console.error('Error in register', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
   login: async (req, res) => {
-    res.send('login');
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json(createError);
+    }
+    try {
+      const user = await prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        return res.status(400).json({ error: 'Неверный логин или пароль' });
+      }
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) {
+        return res.status(400).json({ error: 'Неверный логин или пароль' });
+      }
+      //в токене шифруем id пользователя
+      const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+    } catch (error) {}
   },
   getUserById: async (req, res) => {
     res.send('getUserById');
