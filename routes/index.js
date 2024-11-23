6ad3b9ac9ed4ada85294db4b832502e9d87d347b
MaXmEd <153842541+1MaXmEd1@users.noreply.github.com>
2024-11-23 18:13:47 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+//const multer = require('multer');
 const {
   UserController,
   PostController,
@@ -10,28 +10,34 @@ const {
 } = require('../controllers');
 const { authenticateToken } = require('../middleware/auth');
 
-const uploadDestination = 'uploads';
+//const uploadDestination = 'uploads';
 
 //Показываем где хранить файлы (картинки) так же путь и что файл будет с оригинальным именем
+/*
+
 const storage = multer.diskStorage({
   destination: uploadDestination,
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
+*/
 
-const upload = multer({ storage: storage });
+//const upload = multer({ storage: storage });
 //Роуты пользователя
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/current', authenticateToken, UserController.current);
 router.get('/users/:id', authenticateToken, UserController.getUserById);
+/*
 router.put(
   '/users/:id',
   authenticateToken,
   upload.single('avatar'),
   UserController.updateUser
 );
+*/
+
 //Роуты постов
 router.post('/posts', authenticateToken, PostController.createPost);
 router.get('/posts', authenticateToken, PostController.getAllPosts);
