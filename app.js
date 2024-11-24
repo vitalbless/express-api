09b5573feee772c195a7fbbd2decf1416b9511b2
MaXmEd <153842541+1MaXmEd1@users.noreply.github.com>
2024-11-24 18:38:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
-//Раздача статических файлов из папки 'uploads' она создается автоматически
+
+// Раздача статических файлов из папки 'uploads'
 app.use('uploads', express.static('uploads'));
 
 app.use('/api', require('./routes'));
-//existsSyncs проверяет есть ли какая то папка
+
+// Проверка и создание папки 'uploads', если её нет
 if (!fs.existsSync('uploads')) {
-  //если false то создаем папку
   fs.mkdirSync('uploads');
 }
 
@@ -32,13 +33,22 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // Логируем ошибку в консоль
+  console.error(err.stack);
+
+  // Устанавливаем локальные переменные для ошибки
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Отправляем ответ с ошибкой в браузер
   res.status(err.status || 500);
-  res.render('error');
+  if (req.app.get('env') === 'development') {
+    // В режиме разработки показываем стек ошибки
+    res.render('error', { error: err.stack });
+  } else {
+    // В продакшн-среде показываем только общее сообщение
+    res.render('error', { error: 'Something went wrong!' });
+  }
 });
 
 module.exports = app;
